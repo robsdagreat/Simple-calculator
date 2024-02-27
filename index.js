302f73f064c6d659080df1b1faf6781202619843
robsdagreat <robsrwibutso12@gmail.com>
2024-02-27 22:34:32 +0200

    added arithmetic operation functions

---
+++ index.js
@@ -0,0 +1,17 @@
+const add= (num1 , num2)=>{
+    return num1 + num2;
+  }
+  
+  const substract= (num1 , num2)=>{
+      return num1 - num2;
+    }
+  
+    const multply= (num1 , num2)=>{
+      return num1 * num2;
+    }
+  
+    const divide= (num1 , num2)=>{
+      if(num2 !== 0 ){
+      return num1 / num2;
+  }
+    }
